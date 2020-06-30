11b1d659e5935c81831085bd360aa4798bad9c83
Enor Anidi-Ryz <hello@enoranidi.com>
2020-06-30 15:39:23 +0100

    Updates to prototypes

--- app/views/current/_routes.js
+++ app/views/current/_routes.js
@@ -3,8 +3,9 @@ const router = express.Router()
 
 // Add your routes here - above the module.exports line
 
+// Recorder 
 // 'eligibility-router' page does not exist and use same url for form action
-router.all('/submit-application/eligibility-router', function (req, res) {
+router.all('/submit-application/recorder/eligibility-router', function (req, res) {
 	if (
 		// if the session data called 'qualified' has a value (i.e. it exists) AND the session data called 'qualified' has a value of "no"
 		(req.session.data['qualified-check'] && (req.session.data['qualified-check'] == "no")) || 
@@ -20,7 +21,7 @@ router.all('/submit-application/eligibility-router', function (req, res) {
 		}
 	})
 
-router.all('/submit-application/ethnic-group-router', function (req, res) {
+router.all('/submit-application/recorder/ethnic-group-router', function (req, res) {
 
 	let ethnicGroup = req.session.data['ethnic-group']
 	
@@ -43,7 +44,7 @@ router.all('/submit-application/ethnic-group-router', function (req, res) {
 	}
 })
 
-router.all('/submit-application/caution-conviction-router', function (req, res) {
+router.all('/submit-application/recorder/caution-conviction-router', function (req, res) {
 	
 	let cautionConviction = req.session.data['caution-conviction']
 	
@@ -54,7 +55,7 @@ router.all('/submit-application/caution-conviction-router', function (req, res) 
 	}
 })
 
-router.all('/submit-application/penalty-notice-router', function (req, res) {
+router.all('/submit-application/recorder/penalty-notice-router', function (req, res) {
 	
 	let penaltyNotice = req.session.data['penalty-notice']
 	
@@ -65,7 +66,7 @@ router.all('/submit-application/penalty-notice-router', function (req, res) {
 	}
 })
 
-router.all('/submit-application/driving-offences-router', function (req, res) {
+router.all('/submit-application/recorder/driving-offences-router', function (req, res) {
 	
 	let drivingOffence = req.session.data['driving-conviction']
 	
@@ -76,7 +77,7 @@ router.all('/submit-application/driving-offences-router', function (req, res) {
 	}
 })
 
-router.all('/submit-application/endorsement-notices-router', function (req, res) {
+router.all('/submit-application/recorder/endorsement-notices-router', function (req, res) {
 	
 	let endorsementNotice = req.session.data['endorsement-notice']
 	
@@ -87,7 +88,7 @@ router.all('/submit-application/endorsement-notices-router', function (req, res)
 	}
 })
 
-router.all('/submit-application/declared-bankrupt-router', function (req, res) {
+router.all('/submit-application/recorder/declared-bankrupt-router', function (req, res) {
 	
 	let declaredBankrupt = req.session.data['bankrupt']
 	
@@ -98,7 +99,7 @@ router.all('/submit-application/declared-bankrupt-router', function (req, res) {
 	}
 })
 
-router.all('/submit-application/late-returns-router', function (req, res) {
+router.all('/submit-application/recorder/late-returns-router', function (req, res) {
 	
 	let lateReturns = req.session.data['late-returns']
 	
@@ -109,7 +110,7 @@ router.all('/submit-application/late-returns-router', function (req, res) {
 	}
 })
 
-router.all('/submit-application/professional-proceeds-router', function (req, res) {
+router.all('/submit-application/recorder/professional-proceeds-router', function (req, res) {
 	
 	let professionalProceeds = req.session.data['professional-proceeds']
 	
@@ -120,7 +121,7 @@ router.all('/submit-application/professional-proceeds-router', function (req, re
 	}
 })
 
-router.all('/submit-application/professional-complaints-router', function (req, res) {
+router.all('/submit-application/recorder/professional-complaints-router', function (req, res) {
 	
 	let professionalComplaints = req.session.data['professional-complaints']
 	
@@ -131,7 +132,147 @@ router.all('/submit-application/professional-complaints-router', function (req, 
 	}
 })
 
-router.all('/submit-application/other-issues-router', function (req, res) {
+router.all('/submit-application/recorder/other-issues-router', function (req, res) {
+	
+	let otherIssues = req.session.data['other-issues']
+	
+	if (otherIssues === 'Yes') {
+		res.redirect('other-issues-details')
+	} else {
+		res.redirect('task-list')
+	}
+})
+
+// Senior Circuit Judge 
+// 'eligibility-router' page does not exist and use same url for form action
+router.all('/submit-application/scj/eligibility-router', function (req, res) {
+	if (
+		// if the session data called 'qualified' has a value (i.e. it exists) AND the session data called 'qualified' has a value of "no"
+		(req.session.data['qualified-check'] && (req.session.data['qualified-check'] == "no")) || 
+		(req.session.data['character-check'] && (req.session.data['character-check'] == "no")) ||  
+		(req.session.data['retirement-check'] && (req.session.data['retirement-check'] == "no")) ||  
+		(req.session.data['citizenship-check'] && (req.session.data['citizenship-check'] == "no") 
+  		) 
+	){
+	res.redirect('eligibility-no')
+		} else {
+    	// everything is either blank or yes
+		res.redirect('eligibility-yes')
+		}
+	})
+
+router.all('/submit-application/scj/ethnic-group-router', function (req, res) {
+
+	let ethnicGroup = req.session.data['ethnic-group']
+	
+	if (ethnicGroup === 'Asian or Asian British') {
+		res.redirect('ethnic-group-asian')
+	}
+	else if (ethnicGroup === 'Black, African, Black British or Caribbean') {
+		res.redirect('ethnic-group-black')
+	}
+	else if (ethnicGroup === 'Mixed or multiple ethnic groups') {
+		res.redirect('ethnic-group-mixed')
+	}
+	else if (ethnicGroup === 'White') {
+		res.redirect('ethnic-group-white')
+	} 
+	else if (ethnicGroup === 'Another ethnic group') {
+		res.redirect('ethnic-group-another')
+	} else {
+		res.redirect('gender')
+	}
+})
+
+router.all('/submit-application/scj/caution-conviction-router', function (req, res) {
+	
+	let cautionConviction = req.session.data['caution-conviction']
+	
+	if (cautionConviction === 'Yes') {
+		res.redirect('caution-conviction-details')
+	} else {
+		res.redirect('penalty-notice')
+	}
+})
+
+router.all('/submit-application/scj/penalty-notice-router', function (req, res) {
+	
+	let penaltyNotice = req.session.data['penalty-notice']
+	
+	if (penaltyNotice === 'Yes') {
+		res.redirect('penalty-notice-details')
+	} else {
+		res.redirect('driving-offences')
+	}
+})
+
+router.all('/submit-application/scj/driving-offences-router', function (req, res) {
+	
+	let drivingOffence = req.session.data['driving-conviction']
+	
+	if (drivingOffence === 'Yes') {
+		res.redirect('driving-offences-details')
+	} else {
+		res.redirect('endorsement-notices')
+	}
+})
+
+router.all('/submit-application/scj/endorsement-notices-router', function (req, res) {
+	
+	let endorsementNotice = req.session.data['endorsement-notice']
+	
+	if (endorsementNotice === 'Yes') {
+		res.redirect('endorsement-notices-details')
+	} else {
+		res.redirect('declared-bankrupt')
+	}
+})
+
+router.all('/submit-application/scj/declared-bankrupt-router', function (req, res) {
+	
+	let declaredBankrupt = req.session.data['bankrupt']
+	
+	if (declaredBankrupt === 'Yes') {
+		res.redirect('declared-bankrupt-details')
+	} else {
+		res.redirect('late-returns')
+	}
+})
+
+router.all('/submit-application/scj/late-returns-router', function (req, res) {
+	
+	let lateReturns = req.session.data['late-returns']
+	
+	if (lateReturns === 'Yes') {
+		res.redirect('late-returns-details')
+	} else {
+		res.redirect('professional-proceeds')
+	}
+})
+
+router.all('/submit-application/scj/professional-proceeds-router', function (req, res) {
+	
+	let professionalProceeds = req.session.data['professional-proceeds']
+	
+	if (professionalProceeds === 'Yes') {
+		res.redirect('professional-proceeds-details')
+	} else {
+		res.redirect('professional-complaints')
+	}
+})
+
+router.all('/submit-application/scj/professional-complaints-router', function (req, res) {
+	
+	let professionalComplaints = req.session.data['professional-complaints']
+	
+	if (professionalComplaints === 'Yes') {
+		res.redirect('professional-complaints-details')
+	} else {
+		res.redirect('other-issues')
+	}
+})
+
+router.all('/submit-application/scj/other-issues-router', function (req, res) {
 	
 	let otherIssues = req.session.data['other-issues']
 	
